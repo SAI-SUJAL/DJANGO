382cb62200320ecc450dea9d5b0d579abd874af1
SAI-SUJAL <122451948+SAI-SUJAL@users.noreply.github.com>
2023-07-13 16:51:29 +0530

    views.py

--- hello/views.py
+++ hello/views.py
@@ -8,7 +8,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def append_to_file(request):
+    
     if request.method == 'POST':
+        print("hrrrrr")
         string_to_append = request.POST.get('stringToAppend')
 
         if not string_to_append:
@@ -16,11 +18,13 @@ def append_to_file(request):
 
         data = json.loads(string_to_append)
         keys = data.keys()
+        print(data)
 
         desktop_dir = os.path.join(os.path.expanduser('~'), 'Desktop', 'android')
         file_path = os.path.join(desktop_dir, 'stats.csv')
 
         # Create the directory if it doesn't exist
+        
         if not os.path.exists(desktop_dir):
             os.makedirs(desktop_dir)
 
@@ -31,7 +35,11 @@ def append_to_file(request):
             if is_new_file:
                 writer.writerow(keys)
             writer.writerow([data[key] for key in keys])
+            
 
+        print('String appended to the file successfully.')
         return JsonResponse({'message': 'String appended to the file successfully.'}, status=200)
+    
 
+    print('Invalid request method.')
     return JsonResponse({'error': 'Invalid request method.'}, status=405)
